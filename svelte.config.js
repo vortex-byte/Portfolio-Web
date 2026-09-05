527a5dc1535b4dc2201a7eadf2bcd1a52cd65639
Vortex-Byte <mzimam.ath@gmail.com>
2026-09-05 19:06:29 +0700

    refactor(config): simplify SvelteKit adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: false
-		})
+		adapter: adapter()
 	}
 };
-
-export default config;
